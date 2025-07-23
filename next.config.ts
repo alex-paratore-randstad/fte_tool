1d88e7f70610acccee6a7a77327521fbb7350b7f
Sawyer Ames <sawyer.ames@randstadsourceright.com>
2025-07-23 19:13:41 +0000

    This was my issue when I tried to deploy
    studio-1394982776:~/studio{mas

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: true,
