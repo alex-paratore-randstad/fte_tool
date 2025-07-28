79f7112e517093482370b5588f4959ff5f28dea7
Mydhili Gollapudi <mydhili.gollapudi@randstadsourceright.com>
2025-07-28 16:42:11 +0000

    Currently the page is navigating to page/index.txt instead of page/index

--- next.config.ts
+++ next.config.ts
@@ -27,12 +27,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  trailingSlash: false,
-  generateBuildId: async () => {
-    // Using a timestamp ensures a unique build ID for each deployment,
-    // effectively busting any cache.
-    return new Date().getTime().toString();
-  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
